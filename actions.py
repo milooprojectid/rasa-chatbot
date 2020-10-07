0a8e7f8b4070207d3fec5e96b0a0cf0dc43d3858
Fahmi Salman Nurfikri <fahmisalman@gmail.com>
2020-10-07 12:04:10 +0700

    Update slot filling

--- actions.py
+++ actions.py
@@ -4,35 +4,13 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
 from typing import Any, Text, Dict, List, Union
+import re
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 from rasa_sdk.events import SlotSet
-# from rasa_sdk import Action
 from rasa_sdk.events import UserUtteranceReverted
 
 class RegistForm(FormAction):
@@ -48,86 +26,39 @@ class RegistForm(FormAction):
             "no_telfon"
             ]
 
-    # def run(self, dispatcher, tracker, domain):
-    #     print(tracker.latest_message)
-    #     # tracker.slots["company"] = "Prosa"
-    #     print(tracker.slots)
-    #     # print(domain)
-    #     dispatcher.utter_template("utter_greet", tracker)
-    #     # return [SlotSet("nama", "Fahmi"), SlotSet("email", "Fahmi@a.com"), SlotSet("no_telfon", "09890")]
-    #     return []
-
     def validate(self,
                  dispatcher: CollectingDispatcher,
                  tracker: Tracker,
                  domain: Dict[Text, Any]) -> List[Dict]:
 
+        ask_nama = domain["responses"]["utter_ask_nama"][0]["text"]
+        ask_email = domain["responses"]["utter_ask_email"][0]["text"]
+        ask_telfon = domain["responses"]["utter_ask_no_telfon"][0]["text"]
 
-        ask_nama = "namamu?"
-        ask_email = "emailmu?"
-        ask_telfon = "no telfon?"
-        
-        # print("----------------------------------------------------")
-        # print(dir(dispatcher))
-        # print(dispatcher.messages, dispatcher.utter_message())
-        # print("Wawawawa")
-        # print(tracker.latest_message["text"])
-        # print(tracker.latest_message)
-        # print(dir(tracker))
-        # print(dir(tracker.current_state))
-        # print(tracker.slots, tracker.latest_action_name, tracker.followup_action, tracker.get_latest_input_channel(), tracker.events, tracker.events_after_latest_restart, tracker.active_form)
-        # print(tracker.followup_action, tracker.applied_events(), tracker.slots)
         i = len(tracker.events) - 1
         while i >= 0 and tracker.events[i]["event"] != "bot":
-            print(tracker.events[i]["event"])
             i -= 1
-        # print(tracker.events[i]["event"])
-        # if tracker.events[i]["event"] == "bot":
 
-        #     print(tracker.events[i]["text"])
-        # print(tracker.events[-4])
-        # print(tracker.events[-4]["text"])
-        # print(tracker.latest_message)
         if tracker.events[i]["event"] == "bot":
             if tracker.events[i]["text"] == ask_nama:
+                dispatcher.utter_message("Hai {}".format(tracker.latest_message["text"]))
                 return [SlotSet("nama", tracker.latest_message["text"])]
             if tracker.events[i]["text"] == ask_email:
-                if '@' in tracker.latest_message["text"]:
-                    return [SlotSet("email", tracker.latest_message["text"])]
+                regex_validator = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+                for word in tracker.latest_message["text"].split():
+                    if '@' in tracker.latest_message["text"] and re.search(regex_validator, word):
+                        return [SlotSet("email", word)]
                 else:
                     dispatcher.utter_message("Email yang kamu masukan tidak valid")
             if tracker.events[i]["text"] == ask_telfon:
                 return [SlotSet("no_telfon", tracker.latest_message["text"])]
-            # print("emailnya: ", tracker.latest_message["text"])
 
-        # if "nama" in tracker.latest_message["text"]:
-        #     return [SlotSet("nama", tracker.latest_message["text"])]
-        # elif "email" in tracker.latest_message["text"]:
-        #     return [SlotSet("email", tracker.latest_message["text"])]
-        # elif "telfon" in tracker.latest_message["text"]:
-        #     return [SlotSet("no_telfon", tracker.latest_message["text"])]
-        # if 
-        # slot_values = self.extract_other_slots(dispatcher, tracker, domain)
-        # slot_to_fill = tracker.get_slot(REQUESTED_SLOT)
-
-        # if slot_to_fill:
-        #     slot_values.update(self.extract_requested_slot(dispatcher, tracker, domain))
-
-        # else:
-        #     dispatcher.utter_message("Validation failed")
-
-        # return [SlotSet(slot, value) for slot, value in slot_values.items()]
-        # return [SlotSet("nama", "Fahmi"), SlotSet("email", "Fahmi@a.com"), SlotSet("no_telfon", "09890")]
-        return []
-    
-    def slot_mappings(self):
-        print("masuk slot mapping")
         return []
 
     def submit(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any],) -> List[Dict]:
         
-        print("masuk submit")
-        dispatcher.utter_message("Terima kasih {}, karena sudah mendaftar".format(tracker.slots["nama"]))
+        dispatcher.utter_message("Terima kasih {}, data kamu sudah berhasil disimpan".format(tracker.slots["nama"]))
+        dispatcher.utter_message("Berikut merupakan data yang kamu masukan\nNama: {}\nEmail: {}\nNo.Telfon: {}".format(tracker.slots["nama"], tracker.slots["email"], tracker.slots["no_telfon"]))
         return []
